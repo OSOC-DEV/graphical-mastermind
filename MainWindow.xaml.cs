fef195a2da6c43c3a429ac21917df22244062fc6
16TWindsor <56973230+16TWindsor@users.noreply.github.com>
2019-11-21 15:04:27 +0000

    Cleaned up code Added a seperate file for the Randnum Class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,6 @@ namespace mastermind_gui
     /// </summary>
     public partial class MainWindow : Window
     {
-        int counter;
         public MainWindow()
         {
             InitializeComponent(); //Start MainWindow
@@ -33,13 +32,5 @@ namespace mastermind_gui
         }
 
     }
-    public class Randnum
-    {
-        public int Randint(int low, int high)
-        {
-            Random random = new Random();
-            int randnum = random.Next(low, high);
-            return randnum;
-        }
-    }
+
 }
