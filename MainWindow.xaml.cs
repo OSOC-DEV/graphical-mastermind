27e70ef931cd2f2e3f678c0071d44df880909244
16TWindsor <56973230+16TWindsor@users.noreply.github.com>
2019-11-21 13:35:17 +0000

    Code Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mastermind_gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int counter;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Randnum randnum = new Randnum();
+            label.Content = randnum.Randint(1, 1000);
+        }
+
+    }
+    public class Randnum
+    {
+        public int Randint(int low, int high)
+        {
+            Random random = new Random();
+            int randnum = random.Next(low, high);
+            return randnum;
+        }
+    }
+}
