8732abb87da6f857eab7ec279643db0f40cff4ae
16TWindsor <56973230+16TWindsor@users.noreply.github.com>
2019-11-23 18:25:28 +0000

    Fixed NuGet

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace mastermind_gui
 {
@@ -13,5 +16,10 @@ namespace mastermind_gui
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppCenter.Start("554ad48f-0d48-40af-8879-c86e0e20531b", typeof(Analytics), typeof(Crashes));
+        }
     }
 }
