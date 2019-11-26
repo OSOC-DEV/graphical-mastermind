56731859eb4c84e59af97b0e29e2d0a957c668d7
Epsilon <56973230+Epsilon-E19@users.noreply.github.com>
2019-11-26 08:24:40 +0000

    GUI Completed Backend logic in progress

--- App.xaml.cs
+++ App.xaml.cs
@@ -15,7 +15,7 @@ namespace mastermind_gui
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
+    { 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,7 +25,44 @@ namespace mastermind_gui
 
     public partial class MainWindow : Window
     {
+        readonly Randnum randnum = new Randnum();
         readonly DispatcherTimer timer = new DispatcherTimer();
+        int randomnum = 0;
+
+        int Initialize()
+        {
+            randomnum = randnum.Randint(1000, 9999);
+            return 0;
+        }
+
+        void Checker(string text)
+        {
+                try
+                {
+                    if (Int32.Parse(text) == randomnum)
+                    {
+                        label.Content = "Correct";
+                        Initialize();
+                    }
+                    else if (Int32.Parse(text) > randomnum)
+                    {
+                        label.Content = "Too High";
+                    }
+                    else if (Int32.Parse(text) < randomnum)
+                    {
+                        label.Content = "Too Low";
+                    }
+                    else if (text.Length != 4)
+                    {
+                        label.Content = "Enter a Four digit number";
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    label.Content = "Enter a Four digit number";
+                }
+
+        }
 
         public MainWindow()
         {
@@ -48,8 +85,7 @@ namespace mastermind_gui
             if (progress.Value >= 100)
             {
                 progress.Value = 0;
-                Randnum randnum = new Randnum();
-                label.Content = randnum.Randint(1, 1000);
+                Checker(guess.Text);
                 timer.Stop();
             }
         }
