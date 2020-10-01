b0a30eb39053979574bd54451775910988b8084b
A-NEUN <a.neun@outlook.com>
2020-10-01 14:22:49 +0100

    Code refactor (#6)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
@@ -14,7 +8,7 @@ namespace mastermind_gui
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     { 
         protected override void OnStartup(StartupEventArgs e)
         {
