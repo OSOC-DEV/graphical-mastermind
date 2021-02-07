d93e0c416fbedc709bed1967ec5e9a5bc2f680f8
Tilden Windsor <a.neun@outlook.com>
2021-02-07 20:56:54 +0000

    Added Dotnet steps

--- .space.kts
+++ .space.kts
@@ -6,4 +6,10 @@
 
 job("build") {
     container("mcr.microsoft.com/dotnet/sdk:5.0")
+            shellScript {
+            content = """
+				dotnet restore
+                dotnet build
+            """
+        }
 }
